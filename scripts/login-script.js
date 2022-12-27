cb4ee8ef9061d0b642005667497220ab4a42d966
Vectari <mateusz.majer996@gmail.com>
2022-12-27 01:23:22 +0100

    add login only with existing uername and password

--- scripts/login-script.js
+++ scripts/login-script.js
@@ -1,23 +1,25 @@
-// Registration Button
-let loginBtn = document.getElementById('login-btn');
+// // Login Button
+// let loginBtn = document.getElementById('login-btn');
 
-// Get all registration value
-loginBtn.addEventListener('click', (e) => {
+// // Get all login value
+// loginBtn.addEventListener('click', (e) => {
+//     let username = document.getElementById('username');
+//     let password = document.getElementById('password');
+
+//     if ((username.value === sessionStorage.getItem('username', username.value)) && (password.value === sessionStorage.getItem('password', password.value))) {
+        
+//     }  
+// });
+
+function validate(e){
+    e.preventDefault(); // with preventDefault user can only login with existing username and password
     let username = document.getElementById('username');
     let password = document.getElementById('password');
-    let email = document.getElementById('email');
-    let confirmEmail = document.getElementById('confirm-email');
 
-        // sessionStorage.setItem('username', username.value);
-        // sessionStorage.setItem('password', password.value);
-        // sessionStorage.setItem('email', email.value);
-        // sessionStorage.setItem('confirmEmail', confirmEmail.value);
-
-    if ((username.value.length > 0) && (password.value.length > 0) && (email.value.length > 0) && (confirmEmail.value.length > 0)) {
-        sessionStorage.setItem('username', username.value);
-        sessionStorage.setItem('password', password.value);
-        sessionStorage.setItem('email', email.value);
-        sessionStorage.setItem('confirmEmail', confirmEmail.value);
-    };
-
-});
+    if ((username.value === sessionStorage.getItem('username', username.value)) && (password.value === sessionStorage.getItem('password', password.value))) {
+        alert ("Login successfully");
+        window.location.href = "main.html";
+    } else {
+        alert ("login error");
+    }
+};
