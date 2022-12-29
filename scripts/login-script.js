6449ff6d84973fed100b9b809e821a285218c41a
Vectari <mateusz.majer996@gmail.com>
2022-12-29 22:09:55 +0100

    one user working

--- scripts/login-script.js
+++ scripts/login-script.js
@@ -1,16 +1,3 @@
-// // Login Button
-// let loginBtn = document.getElementById('login-btn');
-
-// // Get all login value
-// loginBtn.addEventListener('click', (e) => {
-//     let username = document.getElementById('username');
-//     let password = document.getElementById('password');
-
-//     if ((username.value === sessionStorage.getItem('username', username.value)) && (password.value === sessionStorage.getItem('password', password.value))) {
-        
-//     }  
-// });
-
 function validate(e){
     e.preventDefault(); // with preventDefault user can only login with existing username and password
     let username = document.getElementById('username');
--- scripts/main-script.js
+++ scripts/main-script.js
@@ -1,3 +1,52 @@
 // Display username near "LOGOUT" button on the right side of navbar
-document.getElementById('username').textContent += `${sessionStorage.getItem('username')}`;
+document.getElementById('username').textContent += `Dzień dobry, ${sessionStorage.getItem('username')}!`;
 
+
+// const url = "https://api.jsonbin.io/v3/b/63a092ab15ab31599e2045be";
+// fetch(url, {
+//     method: "GET",
+//     headers: {
+//          "X-Access-Key": "$2b$10$5pBRUbFRKdKft/b8qSQ3IeyPQgQ8CLXlvgoQA6GdpYvdWva.pOfGS",
+//     }
+// })
+//     .then(response => {
+//         return response.json();
+//     })
+//     .then(json => {
+//         console.log(json);
+//     })
+
+
+    let req = new XMLHttpRequest();
+
+    req.onreadystatechange = () => {
+      if (req.readyState == XMLHttpRequest.DONE) {
+        console.log(req.responseText);
+      }
+    };
+    
+    req.open("GET", "https://api.jsonbin.io/v3/b/63a092ab15ab31599e2045be", true);
+    req.setRequestHeader("X-Master-Key", "<YOUR_API_KEY>");
+    req.send();
+
+
+
+
+// fetch('https://api.jsonbin.io/v3/b/63a092ab15ab31599e2045be', {
+//     method: 'GET',
+//     headers: {
+//         'x-access-key': '$2b$10$5pBRUbFRKdKft/b8qSQ3IeyPQgQ8CLXlvgoQA6GdpYvdWva.pOfGS',
+//         'Content-Type': 'application/json'
+//     },
+    
+
+// })  .then((response) => response.json())
+//     .then((json) => console.log(json))
+
+
+
+//     Dane potrzebne do wyświetlenia widoku aplikacja pobiera przy użyciu fetch API
+// ▪ GET z URL https://api.jsonbin.io/v3/b/63a092ab15ab31599e2045be
+// ▪ nagłówek autoryzacyjny x-access-key o wartości: 
+// $2b$10$5pBRUbFRKdKft/b8qSQ3IeyPQgQ8CLXlvgoQA6GdpYvdWva.pOfGS
+// ▪ odpowiedź zawiera „transactions” oraz „transactionTypes”
