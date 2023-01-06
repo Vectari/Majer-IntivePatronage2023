491f9f5c887fc3151e5a5ab0fdd266a4b50cef4f
Mateusz <vectari@pop-os.localdomain>
2023-01-06 18:53:45 +0100

    add language change to registration.html without reg(error)status

--- scripts/registration-script.js
+++ scripts/registration-script.js
@@ -1,3 +1,34 @@
+// LANGUAGE CHANGE
+let loginButton = document.getElementById("login");
+let registrationTitle = document.getElementById("title-reg");
+let usernameTitle = document.getElementById("username-title");
+let passwordTitle = document.getElementById("password-title");
+let emailTitle = document.getElementById("email-title");
+let confirmEmailTitle = document.getElementById("confirm-email-title");
+let registrationButtonTitle = document.getElementById("reg-btn");
+
+
+function languageENG(e) {
+    sessionStorage.setItem('language', '1');
+    window.location.reload();
+}
+
+function languagePL(e) {
+    sessionStorage.setItem('language', '0');
+    window.location.reload();
+}
+
+if ((sessionStorage.getItem('language')) === "1") {
+    //change language for static element
+    loginButton.innerHTML = `Log In`;
+    registrationTitle.innerHTML = `Registration`;
+    usernameTitle.innerHTML = `Username`;
+    passwordTitle.innerHTML = `Password`;
+    emailTitle.innerHTML = `Email`;
+    confirmEmailTitle.innerHTML = `Confirm Email`;
+    registrationButtonTitle.innerHTML = `Register`;
+}
+
 // Registration Button
 let regBtn = document.getElementById('reg-btn');
 
