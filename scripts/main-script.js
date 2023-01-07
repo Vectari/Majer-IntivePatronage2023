adb3ff1aa0b97baac3f158736b4a453241d3da71
Mateusz <vectari@pop-os.localdomain>
2023-01-07 17:18:15 +0100

    test bar and doughnut charts

--- scripts/main-script.js
+++ scripts/main-script.js
@@ -2,32 +2,32 @@
 document.getElementById('username-welcome').textContent += `Dzień dobry, ${sessionStorage.getItem('username')}!`;
 
 // Fetch API from URL
-const API_URL = "https://api.npoint.io/38edf0c5f3eb9ac768bd";
+// const API_URL = "https://api.npoint.io/38edf0c5f3eb9ac768bd";
 
-let transaction;
-let transactionType;
+// let transaction;
+// let transactionType;
 
-fetch(API_URL)
-    .then((res) => res.json())
-    // .then((res) => console.log(res))
-    .then((transactionList) => {
-        transaction = transactionList.transactions.map((transactions) => {
-            return {
-                date: transactions.date,
-                type: transactions.type,
-                description: transactions.description,
-                amount: transactions.amount,
-                balance: transactions.balance,
-            };
-        });
-        console.log(transaction);
+// fetch(API_URL)
+//     .then((res) => res.json())
+//     // .then((res) => console.log(res))
+//     .then((transactionList) => {
+//         transaction = transactionList.transactions.map((transactions) => {
+//             return {
+//                 date: transactions.date,
+//                 type: transactions.type,
+//                 description: transactions.description,
+//                 amount: transactions.amount,
+//                 balance: transactions.balance,
+//             };
+//         });
+//         console.log(transaction);
 
-        document.getElementById('date').textContent += `${transaction[0].date}`;
-        document.getElementById('type').textContent += `${transaction[0].type}`;
-        document.getElementById('description').textContent += `${transaction[0].description}`;
-        document.getElementById('amount').textContent += `${transaction[0].amount}`;
-        document.getElementById('balance').textContent += `${transaction[0].balance}`;
-    });
+//         document.getElementById('date').textContent += `${transaction[0].date}`;
+//         document.getElementById('type').textContent += `${transaction[0].type}`;
+//         document.getElementById('description').textContent += `${transaction[0].description}`;
+//         document.getElementById('amount').textContent += `${transaction[0].amount}`;
+//         document.getElementById('balance').textContent += `${transaction[0].balance}`;
+//     });
 
 
 
@@ -42,3 +42,52 @@ fetch(API_URL)
     //     });
     //     console.log(transactionType);
     // });
+
+
+
+//CHART SECTION
+// Doughnut chart
+const ctxDoughnut = document.getElementById('transactionChartDoughnut');
+      
+new Chart(ctxDoughnut, {
+  type: 'doughnut',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
+
+// Bar chart
+const ctxBar = document.getElementById('transactionChartBar');
+      
+new Chart(ctxBar, {
+  type: 'bar',
+  data: {
+    labels: ['Test-Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
