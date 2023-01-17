f10224b314d9147daf31793742d41db247d378fd
Vectari <mateusz.majer996@gmail.com>
2023-01-17 23:58:31 +0100

    in registration:  add errors below right input

--- scripts/registration-script.js
+++ scripts/registration-script.js
@@ -43,7 +43,10 @@ regBtn.addEventListener('click', (e) => {
     
     //variable for errors(status)
     let regStatus = document.getElementById("reg-status");
-
+    let regStatusUsername = document.getElementById("reg-status-username");
+    let regStatusPassword = document.getElementById("reg-status-password");
+    let regStatusEmail = document.getElementById("reg-status-email");
+    let regStatusConfEmail = document.getElementById("reg-status-conf-email");
 
     //pattern for email
     const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
@@ -59,71 +62,112 @@ regBtn.addEventListener('click', (e) => {
             sessionStorage.setItem('password', password.value);
             sessionStorage.setItem('email', email.value);
             sessionStorage.setItem('confirmEmail', confirmEmail.value);
+            // clear all error style
+            document.getElementById("username").style.border = ""; //add red border input if any error with this input
+            regStatusUsername.innerHTML = ``;
+            document.getElementById("password").style.border = ""; //clear style for password input when first hav error here but now is ok
+            regStatusPassword.innerHTML = ``;
+            document.getElementById("email").style.border = ""; //clear style for emailinput when first hav error here but now is ok
+            regStatusEmail.innerHTML = ``;
+            document.getElementById("confirm-email").style.border = ""; //clear style for confirmEmail input when first hav error here but now is ok
+            regStatusConfEmail.innerHTML = ``;
         } else {
             regStatus.innerHTML = `<div class="reg-status-ok">Konto utworzone! Logowanie...</div>`;
             sessionStorage.setItem('username', username.value);
             sessionStorage.setItem('password', password.value);
             sessionStorage.setItem('email', email.value);
             sessionStorage.setItem('confirmEmail', confirmEmail.value);
+            // clear all error style
+            document.getElementById("username").style.border = ""; //add red border input if any error with this input
+            regStatusUsername.innerHTML = ``;
+            document.getElementById("password").style.border = ""; //clear style for password input when first hav error here but now is ok
+            regStatusPassword.innerHTML = ``;
+            document.getElementById("email").style.border = ""; //clear style for emailinput when first hav error here but now is ok
+            regStatusEmail.innerHTML = ``;
+            document.getElementById("confirm-email").style.border = ""; //clear style for confirmEmail input when first hav error here but now is ok
+            regStatusConfEmail.innerHTML = ``;
         }
     } else if ((username.value.length < 6) || (username.value.length > 16) || (!username.value.match(usernameFormat))) {
         if ((sessionStorage.getItem('language')) === "1") {
-            regStatus.innerHTML = `<div class="reg-status-not-ok">Username should be between 6 and 16 characters, it can consist of only letters, numbers and characters - _ [ ] \ / and must contain at least 5 letters and 1 number.</div>`;   //change language for non-static element
+            regStatusUsername.innerHTML = `<div class="reg-status-not-ok">Username should be between 6 and 16 characters, it can consist of only letters, numbers and characters - _ [ ] \ / and must contain at least 5 letters and 1 number.</div>`;   //change language for non-static element
             document.getElementById("username").style.border = "2px solid #FF0000"; //add red border input if any error with this input
             document.getElementById("password").style.border = ""; //clear style for password input when first hav error here but now is ok
+            regStatusPassword.innerHTML = ``;
             document.getElementById("email").style.border = ""; //clear style for emailinput when first hav error here but now is ok
+            regStatusEmail.innerHTML = ``;
             document.getElementById("confirm-email").style.border = ""; //clear style for confirmEmail input when first hav error here but now is ok
+            regStatusConfEmail.innerHTML = ``;
         } else {
-            regStatus.innerHTML = `<div class="reg-status-not-ok">Nazwa użytkownika powinna mieć od 6 do 16 znaków, może składać się z samych liter i cyfr oraz znaków - _ [ ] \ / przy czym musi zawierać najmniej 5 liter i 1 cyfrę.</div>`;
+            regStatusUsername.innerHTML = `<div class="reg-status-not-ok">Nazwa użytkownika powinna mieć od 6 do 16 znaków, może składać się z samych liter i cyfr oraz znaków - _ [ ] \ / przy czym musi zawierać najmniej 5 liter i 1 cyfrę.</div>`;
             document.getElementById("username").style.border = "2px solid #FF0000"; //add red border input if any error with this input
             document.getElementById("password").style.border = ""; //clear style for password input when first hav error here but now is ok
+            regStatusPassword.innerHTML = ``;
             document.getElementById("email").style.border = ""; //clear style for emailinput when first hav error here but now is ok
+            regStatusEmail.innerHTML = ``;
             document.getElementById("confirm-email").style.border = ""; //clear style for confirmEmail input when first hav error here but now is ok
+            regStatusConfEmail.innerHTML = ``;
         }
     } else if (password.value.length < 6) {
         if ((sessionStorage.getItem('language')) === "1") {
-            regStatus.innerHTML = `<div class="reg-status-not-ok">Password must be at least 6 characters.</div>`;   //change language for non-static element
+            regStatusPassword.innerHTML = `<div class="reg-status-not-ok">Password must be at least 6 characters.</div>`;   //change language for non-static element
             document.getElementById("username").style.border = ""; //add red border input if any error with this input
+            regStatusUsername.innerHTML = ``;
             document.getElementById("password").style.border = "2px solid #FF0000"; //clear style for password input when first hav error here but now is ok
             document.getElementById("email").style.border = ""; //clear style for emailinput when first hav error here but now is ok
+            regStatusEmail.innerHTML = ``;
             document.getElementById("confirm-email").style.border = ""; //clear style for confirmEmail input when first hav error here but now is ok
+            regStatusConfEmail.innerHTML = ``;
         } else {
-            regStatus.innerHTML = `<div class="reg-status-not-ok">Hasło musi mieć conajmniej 6 znaków.</div>`;
+            regStatusPassword.innerHTML = `<div class="reg-status-not-ok">Hasło musi mieć conajmniej 6 znaków.</div>`;
             document.getElementById("username").style.border = ""; //add red border input if any error with this input
+            regStatusUsername.innerHTML = ``;
             document.getElementById("password").style.border = "2px solid #FF0000"; //clear style for password input when first hav error here but now is ok
             document.getElementById("email").style.border = ""; //clear style for emailinput when first hav error here but now is ok
+            regStatusEmail.innerHTML = ``;
             document.getElementById("confirm-email").style.border = ""; //clear style for confirmEmail input when first hav error here but now is ok
+            regStatusConfEmail.innerHTML = ``;
         }
     } else if (!email.value.match(mailFormat)) {
         if ((sessionStorage.getItem('language')) === "1") {
-            regStatus.innerHTML = `<div class="reg-status-not-ok">Please enter a valid email address.</div>`;   //change language for non-static element
+            regStatusEmail.innerHTML = `<div class="reg-status-not-ok">Please enter a valid email address.</div>`;   //change language for non-static element
             document.getElementById("username").style.border = ""; //add red border input if any error with this input
+            regStatusUsername.innerHTML = ``;
             document.getElementById("password").style.border = ""; //clear style for password input when first hav error here but now is ok
+            regStatusPassword.innerHTML = ``;
             document.getElementById("email").style.border = "2px solid #FF0000"; //clear style for emailinput when first hav error here but now is ok
             document.getElementById("confirm-email").style.border = "2px solid #FF0000"; //clear style for confirmEmail input when first hav error here but now is ok
+            regStatusConfEmail.innerHTML = ``;
         } else {
-            regStatus.innerHTML = `<div class="reg-status-not-ok">Wpisz poprawny adres email.</div>`;
+            regStatusEmail.innerHTML = `<div class="reg-status-not-ok">Wpisz poprawny adres email.</div>`;
             document.getElementById("username").style.border = ""; //add red border input if any error with this input
+            regStatusUsername.innerHTML = ``;
             document.getElementById("password").style.border = ""; //clear style for password input when first hav error here but now is ok
+            regStatusPassword.innerHTML = ``;
             document.getElementById("email").style.border = "2px solid #FF0000"; //clear style for emailinput when first hav error here but now is ok
             document.getElementById("confirm-email").style.border = "2px solid #FF0000"; //clear style for confirmEmail input when first hav error here but now is ok
+            regStatusConfEmail.innerHTML = ``;
         }
     } else if (email.value !== confirmEmail.value) {
         if ((sessionStorage.getItem('language')) === "1") {
-            regStatus.innerHTML = `<div class="reg-status-not-ok">Email addresses must be identical.</div>`;   //change language for non-static element
+            regStatusEmail.innerHTML = `<div class="reg-status-not-ok">Email addresses must be identical.</div>`;   //change language for non-static element
+            regStatusConfEmail.innerHTML = `<div class="reg-status-not-ok">Email addresses must be identical.</div>`;   //change language for non-static element
             document.getElementById("username").style.border = ""; //add red border input if any error with this input
+            regStatusUsername.innerHTML = ``;
             document.getElementById("password").style.border = ""; //clear style for password input when first hav error here but now is ok
+            regStatusPassword.innerHTML = ``;
             document.getElementById("email").style.border = "2px solid #FF0000"; //clear style for emailinput when first hav error here but now is ok
             document.getElementById("confirm-email").style.border = "2px solid #FF0000"; //clear style for confirmEmail input when first hav error here but now is ok
         } else {
-            regStatus.innerHTML = `<div class="reg-status-not-ok">Adresy email muszą być identyczne.</div>`;
+            regStatusEmail.innerHTML = `<div class="reg-status-not-ok">Adresy email muszą być identyczne.</div>`;
+            regStatusConfEmail.innerHTML = `<div class="reg-status-not-ok">Adresy email muszą być identyczne.</div>`;
             document.getElementById("username").style.border = ""; //add red border input if any error with this input
+            regStatusUsername.innerHTML = ``;
             document.getElementById("password").style.border = ""; //clear style for password input when first hav error here but now is ok
+            regStatusPassword.innerHTML = ``;
             document.getElementById("email").style.border = "2px solid #FF0000"; //clear style for emailinput when first hav error here but now is ok
             document.getElementById("confirm-email").style.border = "2px solid #FF0000"; //clear style for confirmEmail input when first hav error here but now is ok
         }
     } 
-
 });
 
 
