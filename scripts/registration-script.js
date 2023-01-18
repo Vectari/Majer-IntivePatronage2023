e3340ad331bdb4ce40b2f1182404a145bd05d3ff
Vectari <mateusz.majer996@gmail.com>
2023-01-18 22:06:51 +0100

    fix white space on bottom of background img  and add to render \  to username error

--- scripts/registration-script.js
+++ scripts/registration-script.js
@@ -89,7 +89,7 @@ regBtn.addEventListener('click', (e) => {
         }
     } else if ((username.value.length < 6) || (username.value.length > 16) || (!username.value.match(usernameFormat))) {
         if ((sessionStorage.getItem('language')) === "1") {
-            regStatusUsername.innerHTML = `<div class="reg-status-not-ok">Username should be between 6 and 16 characters, it can consist of only letters, numbers and characters - _ [ ] \ / and must contain at least 5 letters and 1 number.</div>`;   //change language for non-static element
+            regStatusUsername.innerHTML = `<div class="reg-status-not-ok">Username should be between 6 and 16 characters, it can consist of only letters, numbers and characters - _ [ ] \\ / and must contain at least 5 letters and 1 number.</div>`;   //change language for non-static element
             document.getElementById("username").style.border = "2px solid #FF0000"; //add red border input if any error with this input
             document.getElementById("password").style.border = ""; //clear style for password input when first hav error here but now is ok
             regStatusPassword.innerHTML = ``;
@@ -98,7 +98,7 @@ regBtn.addEventListener('click', (e) => {
             document.getElementById("confirm-email").style.border = ""; //clear style for confirmEmail input when first hav error here but now is ok
             regStatusConfEmail.innerHTML = ``;
         } else {
-            regStatusUsername.innerHTML = `<div class="reg-status-not-ok">Nazwa użytkownika powinna mieć od 6 do 16 znaków, może składać się z samych liter i cyfr oraz znaków - _ [ ] \ / przy czym musi zawierać najmniej 5 liter i 1 cyfrę.</div>`;
+            regStatusUsername.innerHTML = `<div class="reg-status-not-ok">Nazwa użytkownika powinna mieć od 6 do 16 znaków, może składać się z samych liter i cyfr oraz znaków - _ [ ] \\ / przy czym musi zawierać najmniej 5 liter i 1 cyfrę.</div>`;
             document.getElementById("username").style.border = "2px solid #FF0000"; //add red border input if any error with this input
             document.getElementById("password").style.border = ""; //clear style for password input when first hav error here but now is ok
             regStatusPassword.innerHTML = ``;
